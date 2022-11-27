c21464d314e6bcca6f34652d4272d1dcdcbeb42c
VictoriaQuintero <victoria170460@gmail.com>
2022-11-27 16:21:20 -0400

    Catalogo de telefonos

--- frontend/src/views/telefonosventa.vue
+++ frontend/src/views/telefonosventa.vue
@@ -1,13 +1,104 @@
 <template>
-    <div class="mt-5">
-        <p>lorem ipsum dolor sit amet</p>
+    <div class="div_principal mt-5 d-flex">
+
+        <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/logo.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><a href="#" class="btn btn-primary">COMPRAR</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/logo.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><a href="#" class="btn btn-primary">COMPRAR</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/logo.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><a href="#" class="btn btn-primary">COMPRAR</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/logo.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><a href="#" class="btn btn-primary">COMPRAR</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/logo.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><a href="#" class="btn btn-primary">COMPRAR</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/logo.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><a href="#" class="btn btn-primary">COMPRAR</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <p>lorem ipsum dolor sit amet</p>
         <ul v-for="i of articles" :key="i">
             <li>
                 nombre: {{ i.titulo }} <br>
                  descripcion: {{ i.description }} <br> link:
                 {{ i.img_url }}
             </li>
-        </ul>
+        </ul> -->
     </div>
 </template>
 
@@ -32,3 +123,15 @@ export default {
     },
 };
 </script>
+<style scoped>
+.div_principal {
+    background-color: #06283D;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 5%;
+}
+.card{
+    margin:10px;
+}
+</style>
